ef7f93106884a9a23660be044673c683422974db
Anubhav Marwaha <anubhavmarwaha2000@gmail.com>
2019-05-24 02:33:54 +0530

    url and views made for posting

--- bloggy/urls.py
+++ bloggy/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from .import views
 
-urlpatterns = [path('', views.index, name='index')]
+urlpatterns = [path('', views.index, name='index'), path('', views.post_list, name='post_list'), ]
--- bloggy/views.py
+++ bloggy/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Welcome to the bloggy app')
-    
+    return HttpResponse(request, 'Welcome to the bloggy app')
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
