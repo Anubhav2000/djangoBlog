cbae705bdfc91eaf80415229e7f42ec8693937e1
Anubhav Marwaha <anubhavmarwaha2000@gmail.com>
2019-05-24 13:30:27 +0530

    posts deployed

--- bloggy/models.py
+++ bloggy/models.py
@@ -3,11 +3,12 @@ from django.conf import settings
 from django.utils import timezone
 
 class post(Models.Model):
+    id = Models.AutoField(primary_key=True)
     author = Models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=Models.CASCADE)
     title = Models.CharField(max_length=100)
     text = Models.TextField()
-    createdDate = Models.DateTimeField()
-    publishedDate = Models.DateTimeField()
+    createdDate = Models.DateTimeField(auto_now=True)
+    publishedDate = Models.DateTimeField(auto_now=True)
 
     def publish(self):
         self.publishedDate = timezone.now()
--- bloggy/views.py
+++ bloggy/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import post
+from django.utils import timezone
 
 def index(request):
     return HttpResponse('Welcome to the bloggy app')
 
 def post_list(request):
-    return render(request, 'bloggy/post_list.html', {})
+    posts = post.objects.filter(publishedDate__lte=timezone.now()).order_by('publishedDate')
+    return render(request, 'bloggy/post_list.html', {'posts': posts})
 # Create your views here.
